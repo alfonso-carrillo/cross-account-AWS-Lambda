a11d771749608cb7d8b10cb5b00430ffb05ca426
Alfonso Carrillo <alfonso.carrillo@alphacredit.mx>
2020-05-31 20:11:08 -0500

    codigo de ejemlo para ejecutar una lambda externa

--- CrossAccountFunction/app.js
+++ CrossAccountFunction/app.js
@@ -1,6 +1,25 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
+
+// en caso de que tu lambda esté enotra region 
+// reemplazar por el código de la línea 2 poniendo la region necesaria
+/*const lambda = new AWS.Lambda({
+    region: 'us-east-1'
+});*/
+
+const exFunction = (params) => {
+    return new Promise((resolve, reject) => {
+        lambda.invoke(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                reject(err);
+            }
+            else{
+                resolve(data);
+            }
+        });
+    });
+};
 
 /**
  *
@@ -17,17 +36,22 @@ let response;
 exports.lambdaHandler = async (event, context) => {
     try {
         // const ret = await axios(url);
-        response = {
+
+        let params = {
+            FunctionName: 'arn:aws:lambda:REGION:ID_ACCOUN:function:LambdaAccountA',
+            Payload: ''
+        };
+
+        let response = await exFunction(params);
+
+        console.log('Response Bontu Lambda: ', response);
+
+        return {
             'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
+            'body': response
         }
     } catch (err) {
         console.log(err);
         return err;
     }
-
-    return response
 };
